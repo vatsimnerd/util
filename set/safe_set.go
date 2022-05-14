a798da6a1be82c5c8c67289d53f0291337171b2e
Vincent Elphick <elphickvincent@gmail.com>
2022-05-14 09:48:52 +0100

    thread-safe sets

---
+++ set/safe_set.go
@@ -0,0 +1,114 @@
+package set
+
+import "sync"
+
+type SafeSet[K comparable] struct {
+	m    map[K]bool
+	lock sync.RWMutex
+}
+
+func NewSafe[K comparable]() *SafeSet[K] {
+	return &SafeSet[K]{m: make(map[K]bool)}
+}
+
+func FromListSafe[K comparable](list []K) *SafeSet[K] {
+	m := make(map[K]bool)
+	for _, value := range list {
+		m[value] = true
+	}
+	return &SafeSet[K]{m: m}
+}
+
+func (s *SafeSet[K]) Add(value K) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.m[value] = true
+}
+
+func (s *SafeSet[K]) Delete(value K) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	delete(s.m, value)
+}
+
+func (s *SafeSet[K]) Has(value K) bool {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	_, found := s.m[value]
+	return found
+}
+
+func (s *SafeSet[K]) List() []K {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	list := make([]K, len(s.m))
+	i := 0
+	for value := range s.m {
+		list[i] = value
+		i++
+	}
+	return list
+}
+
+func (s *SafeSet[K]) Iter(callback func(value K)) {
+	values := s.List()
+	for _, value := range values {
+		callback(value)
+	}
+}
+
+func (s *SafeSet[K]) IterIndex(callback func(value K, i int)) {
+	values := s.List()
+	for i, value := range values {
+		callback(value, i)
+	}
+}
+
+func (s *SafeSet[K]) Size() int {
+	return len(s.m)
+}
+
+func (s *SafeSet[K]) Union(other *SafeSet[K]) *SafeSet[K] {
+	res := NewSafe[K]()
+	s.Iter(func(value K) { res.Add(value) })
+	other.Iter(func(value K) { res.Add(value) })
+	return res
+}
+
+func (s *SafeSet[K]) Intersection(other *SafeSet[K]) *SafeSet[K] {
+	if s.Size() > other.Size() {
+		s, other = other, s
+	}
+
+	res := NewSafe[K]()
+	s.Iter(func(value K) {
+		if other.Has(value) {
+			res.Add(value)
+		}
+	})
+
+	return res
+}
+
+func (s *SafeSet[K]) Copy() *SafeSet[K] {
+	res := NewSafe[K]()
+	for k := range s.m {
+		res.m[k] = true
+	}
+	return res
+}
+
+func (s *SafeSet[K]) Subtract(other *SafeSet[K]) *SafeSet[K] {
+	res := NewSafe[K]()
+	values := s.List()
+	for _, k := range values {
+		if !other.Has(k) {
+			res.m[k] = true
+		}
+	}
+	return res
+}
+
+func (s *SafeSet[K]) Difference(other *SafeSet[K]) *SafeSet[K] {
+	return s.Subtract(other)
+}
